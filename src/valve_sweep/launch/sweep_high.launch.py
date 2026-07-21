e16b32136f4eabc831fc03f6b0f5b7aef63de87f
kklab-common <kklab-common@kklab.com>
2026-07-21 14:41:29 +0900

    aaaa

--- src/valve_sweep/launch/sweep_high.launch.py
+++ src/valve_sweep/launch/sweep_high.launch.py
@@ -40,8 +40,8 @@ def generate_launch_description():
 
     return LaunchDescription([
         DeclareLaunchArgument('record_bag', default_value='true'),
-        DeclareLaunchArgument('v_start', default_value='5.0'),
-        DeclareLaunchArgument('v_end', default_value='6.7'),
+        DeclareLaunchArgument('v_start', default_value='6.0'),
+        DeclareLaunchArgument('v_end', default_value='8.0'),
         DeclareLaunchArgument('v_step', default_value='0.1'),
         DeclareLaunchArgument('hold_time_s', default_value='1.0'),
         DeclareLaunchArgument('recharge_time_s', default_value='1.5'),
